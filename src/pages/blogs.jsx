5f634817953f7be3b8f8c7e72d275cb545d833ca
Imad <uimad87@gmail.com>
2026-01-06 21:19:30 +0500

    new feat added

--- src/pages/blogs.jsx
+++ src/pages/blogs.jsx
@@ -5,6 +5,7 @@ export default function Blogs() {
     <div className="flex flex-col items-center justify-center gap-4">
       <div className="font-semibold text-xl">Blog Page</div>
       <h2 className="font-bold text-2xl text-gray-500">CominG SooN!</h2>
+      <h2 className="font-bold text-2xl text-gray-500">CominG SooN!</h2>
     </div>
   );
 }
